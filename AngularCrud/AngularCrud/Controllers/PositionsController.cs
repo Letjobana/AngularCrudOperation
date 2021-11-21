676cc98a60b4e8c18c5f6f6b0b194ab73c2eaa4e
Letjobana <suzanm@gerbertech.co.za>
2021-11-21 20:19:18 +0200

    Added position controller

---
+++ AngularCrud/AngularCrud/Controllers/PositionsController.cs
@@ -0,0 +1,27 @@
+﻿using AngularCrud.Models;
+using AngularCrud.Repositories.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AngularCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PositionsController : ControllerBase
+    {
+        private readonly IPositionService _positionService;
+
+        public PositionsController(IPositionService positionService)
+        {
+            _positionService = positionService;
+        }
+
+        // GET: api/Positions
+        [HttpGet]
+        public async Task<IEnumerable<Positions>> Get()
+        {
+            return await _positionService.GetPositionsList();
+        }
+    }
+}
